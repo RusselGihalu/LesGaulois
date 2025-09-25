6856a4743e8580d9f44d530cfc66d6d664fe2788
RusselGihalu <gihalurussel@gmail.com>
2025-09-25 13:46:32 +0200

    Classe Gaulois est maitenant complet

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,9 +1,28 @@
 package personnages;
 
 public class Gaulois {
+private String nom;
+private int force ;
+	
+ 
+ 
+	public Gaulois(String nom, int force) {
+		this.nom = nom ;
+		this.force = force ;
+		 
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "\" "+ texte + "\"");
+	}
 
-	public Gaulois() {
-		// TODO Auto-generated constructor stub
+	private String prendreParole() {
+		 
+		return "Le gaulois " + nom + " : ";
 	}
 
 }
