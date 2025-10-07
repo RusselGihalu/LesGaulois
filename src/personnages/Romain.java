30dd220cef1099a07ffade8dc2cb196c461d7991
RusselGihalu <gihalurussel@gmail.com>
2025-10-07 08:44:16 +0200

    invariant pre post conditions

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -2,39 +2,71 @@ package personnages;
 
 public class Romain {
 
-	private String nom ;
-	private int force ; 
-	
+	private String nom;
+	private int force;
+
 	public Romain(String nom, int force) {
-		this.nom = nom ;
-		this.force = force ;
+		this.nom = nom;
+		this.force = force;
+		System.out.println("Force Romain Positf : " + this.isIvariantVerified());
+		
 	}
-	
+
 	public String getNom() {
-		return nom ;
+		return nom;
 	}
-	
+
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "\"" + texte + "\"");
 	}
 
 	private String prendreParole() {
-		 
+
 		return "Le romain " + nom + " : ";
 	}
-   
+
 	public void recevoirCoup(int forceCoup) {
 		
-		this.force =  this.force - forceCoup;
+		int forceInitiale = this.force;
 		
-		if (this.force<1) {
+		// Prés condition forceCoup doit etre positif
+		if(force>0) {
+			
+		this.force = this.force - forceCoup;
+
+		if (this.force < 1) {
 			this.parler("j'abandonne !");
-			
-		}else {
-			
+
+		} else {
+
 			this.parler("Aie");
-			
+
 		}
 		
+		}
+		
+		// Postcondition Force diminue
+		
+		if(this.force < forceCoup) {
+			System.out.println("La force du Romain " + this.nom + " a diminuée.");
+			
+		}else {
+			System.out.println("La force du Romain " + this.nom + " n'a pas diminuée.");
+		}
+		
+		
+		System.out.println("Force Romain Positf : " + this.isIvariantVerified());
 	}
+	
+
+	public boolean isIvariantVerified() {
+			return this.force == 0 || this.force>0 ;	
+	}
+	
+	public static void main(String [] args) {
+		
+		Romain minus = new Romain("Minus", 6); 
+		
+	}
+	
 }
